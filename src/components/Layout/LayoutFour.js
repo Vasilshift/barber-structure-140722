975877fa158b6242f650e847213aa9f064c97457
Evgenii <vasilkalin@gmail.com>
2022-07-13 18:42:16 +0300

    container didn't run on servern and I changed some files

--- src/components/Layout/LayoutFour.js
+++ src/components/Layout/LayoutFour.js
@@ -30,9 +30,9 @@ const LayoutFour = ( ) => {
                         href="/shop/left-sidebar"
                         as={process.env.PUBLIC_URL + "/shop/left-sidebar"}
                     >
-                      <a className="lezada-button lezada-button--medium lookbook-btn">
-                        shop now
-                      </a>
+                      {/*<a className="lezada-button lezada-button--medium lookbook-btn">*/}
+                      {/*  shop now*/}
+                      {/*</a>*/}
                     </a>
                   </div>
                 </div>
@@ -55,18 +55,18 @@ const LayoutFour = ( ) => {
             <Row>
               <Col md={8} lg={7} className="ml-auto">
                 <div className="lookbook-content-wrapper d-flex flex-column justify-content-center">
-                  <div className="lookbook-content lookbook-content--dark text-center">
-                    <p className="tag mytag">#BEACHWEAR #SUMMER</p>
-                    <h2 className="lookbook-title">Beachwear</h2>
-                    {/*<Link*/}
-                    {/*    href="/shop/left-sidebar"*/}
-                    {/*    as={process.env.PUBLIC_URL + "/shop/left-sidebar"}*/}
-                    {/*>*/}
-                    {/*  <a className="lezada-button lezada-button--medium lookbook-btn">*/}
-                    {/*    shop now*/}
-                    {/*  </a>*/}
-                    {/*</Link>*/}
-                  </div>
+                  {/*<div className="lookbook-content lookbook-content--dark text-center">*/}
+                  {/*  <p className="tag mytag">#BEACHWEAR #SUMMER</p>*/}
+                  {/*  <h2 className="lookbook-title">Beachwear</h2>*/}
+                  {/*  /!*<Link*!/*/}
+                  {/*  /!*    href="/shop/left-sidebar"*!/*/}
+                  {/*  /!*    as={process.env.PUBLIC_URL + "/shop/left-sidebar"}*!/*/}
+                  {/*  /!*>*!/*/}
+                  {/*  /!*  <a className="lezada-button lezada-button--medium lookbook-btn">*!/*/}
+                  {/*  /!*    shop now*!/*/}
+                  {/*  /!*  </a>*!/*/}
+                  {/*  /!*</Link>*!/*/}
+                  {/*</div>*/}
                 </div>
               </Col>
             </Row>
