30ae0cb17923be104dbcf2d73ee2ba2ffd5e3cd3
Evgenii <vasilkalin@gmail.com>
2022-07-15 01:54:10 +0300

    Try add menu from lesson Youtube https://www.youtube.com/watch?v=HE8jtK4FSZY
    https://github.com/mtajammulzia/navbar-dropdown/blob/main/App.js

---
+++ src/components/Header/MyHeader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function Navbar() {
+    return (
+        <>
+            <nav className={"navbar"}>
+                <Link>
+
+
+                </Link>
+            </nav>
+
+
+
+        </>
+    )
+
+
+}
